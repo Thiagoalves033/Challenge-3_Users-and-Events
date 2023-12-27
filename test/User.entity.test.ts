20e39c339f79a12996e57140293945e415f3e2d0
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-27 14:01:26 -0300

    test: revise user entity tests for recent modifications

--- test/User.entity.test.ts
+++ test/User.entity.test.ts
@@ -9,12 +9,12 @@ describe('User entity', () => {
       city: 'city',
       country: 'country',
       email: 'email',
-      password: 'password',
-      confirmPassword: 'password'
+      password: 'password'
     };
 
     const user = new User(userInput);
 
+    expect(user).toHaveProperty('id');
     expect(user).toHaveProperty('firstName', 'first name');
     expect(user).toHaveProperty('lastName', 'last name');
     expect(user).toHaveProperty('birthDate', 'birthdate');
@@ -22,6 +22,21 @@ describe('User entity', () => {
     expect(user).toHaveProperty('country', 'country');
     expect(user).toHaveProperty('email', 'email');
     expect(user).toHaveProperty('password', 'password');
-    expect(user).toHaveProperty('confirmPassword', 'password');
+  });
+
+  it('Should have specified id', () => {
+    const userInput: UserProps = {
+      firstName: 'first name',
+      lastName: 'last name',
+      birthDate: 'birthdate',
+      city: 'city',
+      country: 'country',
+      email: 'email',
+      password: 'password'
+    };
+
+    const user = new User(userInput, 'id');
+
+    expect(user.id).toBe('id');
   });
 });

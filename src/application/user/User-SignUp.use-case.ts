b0783b87b8c94de67fefe339eae96236ffc7d89a
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-27 14:05:14 -0300

    feat: create general UseCase interface and implement signUp usecase

---
+++ src/application/user/User-SignUp.use-case.ts
@@ -0,0 +1,23 @@
+import User from '../../domain/User.entity';
+import IUserRepository from '../../infrastructure/User.repository';
+import IPasswordEncrypter from '../PasswordEncrypter.port';
+import UseCase from '../UseCase';
+
+export default class UserSignUp implements UseCase<User, void> {
+  constructor(
+    private UserRepo: IUserRepository,
+    private Encrypter: IPasswordEncrypter
+  ) {}
+
+  async execute(input: User): Promise<void> {
+    const existingUser = await this.UserRepo.findById(input.id);
+
+    if (existingUser) throw new Error('Email already in use');
+
+    input.password = await this.Encrypter.encrypt(input.password);
+
+    const newUser = new User(input);
+
+    await this.UserRepo.insert(newUser);
+  }
+}

ea6e6990b72fbf7cd2165e4c46468991e21a2207
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-28 20:20:01 -0300

    feat: add new find and delete methods to event repository

--- src/core/ports/repositories/Event.repository.ts
+++ src/core/ports/repositories/Event.repository.ts
@@ -1,6 +1,15 @@
 import Event from '../../entities/Event.entity';
 
+type QueryObject = {
+  filter?: any;
+  sort?: any;
+  limit?: number;
+  offset?: number;
+};
 export default interface IEventRepository {
   insert(event: Event): Promise<void>;
+  findAll(options: QueryObject): Promise<Event[]>;
   findById(id: string): Promise<Event | null>;
+  deleteMany(): Promise<void>;
+  deleteById(id: string): Promise<void>;
 }

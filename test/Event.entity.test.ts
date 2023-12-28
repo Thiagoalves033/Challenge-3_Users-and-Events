24cf9b0019b1a1bb6466494cc519d273fa52c8c1
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-28 00:59:02 -0300

    test: add tests for event entity

---
+++ test/Event.entity.test.ts
@@ -0,0 +1,40 @@
+import Event, { EventProps } from '../src/core/entities/Event.entity';
+
+describe('Event entity', () => {
+  it('Should have the correct properties for an event', () => {
+    const eventInput: EventProps = {
+      description: 'A new event',
+      dayOfWeek: 'sunday'
+    };
+
+    const event = new Event(eventInput);
+
+    expect(event).toHaveProperty('id');
+    expect(event).toHaveProperty('description', 'A new event');
+    expect(event).toHaveProperty('dayOfWeek', 'sunday');
+    expect(event).toHaveProperty('userId', undefined);
+  });
+
+  it('Should have the specified id', () => {
+    const eventInput: EventProps = {
+      description: 'A new event',
+      dayOfWeek: 'sunday'
+    };
+
+    const event = new Event(eventInput, 'id');
+
+    expect(event.id).toBe('id');
+  });
+
+  it('Should set the specified userId', () => {
+    const eventInput: EventProps = {
+      description: 'A new event',
+      dayOfWeek: 'sunday'
+    };
+
+    const event = new Event(eventInput, 'id');
+    event.setUserId('id');
+
+    expect(event.userId).toBe('id');
+  });
+});

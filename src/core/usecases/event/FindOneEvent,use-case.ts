ee86fdbdbb4427f0769e8f0c7e06b1ff6953f76d
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-28 20:23:38 -0300

    feat: implement FindOneEvent usecase

---
+++ src/core/usecases/event/FindOneEvent,use-case.ts
@@ -0,0 +1,28 @@
+import UseCase from '../../ports/interfaces/UseCase';
+import IEventRepository from '../../ports/repositories/Event.repository';
+
+type FindOneOutput = {
+  id: string;
+  description: string;
+  dayOfWeek: string;
+  userId: string;
+};
+
+export default class FindOneEvent implements UseCase<string, FindOneOutput> {
+  constructor(private EventRepo: IEventRepository) {}
+
+  async execute(eventId: string): Promise<FindOneOutput> {
+    const event = await this.EventRepo.findById(eventId);
+
+    if (!event) throw new Error('Could not find event');
+
+    const output: FindOneOutput = {
+      id: event.id,
+      description: event.description,
+      dayOfWeek: event.dayOfWeek,
+      userId: event.userId as string
+    };
+
+    return output;
+  }
+}

c445944ac8c64ecc60171b410314199f4353a33f
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-28 23:54:27 -0300

    feat: add deletion list to DeleteManyEvents

--- src/core/usecases/event/DeleteManyEvents.use-case.ts
+++ src/core/usecases/event/DeleteManyEvents.use-case.ts
@@ -1,22 +1,29 @@
 import UseCase from '../../ports/interfaces/UseCase';
 import IEventRepository from '../../ports/repositories/Event.repository';
 
-export default class DeleteManyEvents implements UseCase<void, void> {
+type DeleteManyOutput = {
+  id: string;
+  description: string;
+  dayOfWeek: string;
+  userId: string;
+};
+export default class DeleteManyEvents implements UseCase<object, DeleteManyOutput[]> {
   constructor(private EventRepo: IEventRepository) {}
 
-  async execute(): Promise<void> {
-    await this.EventRepo.deleteMany();
-    // const output: DeleteManyOutput[] = [];
+  async execute(options: object): Promise<DeleteManyOutput[]> {
+    const events = await this.EventRepo.deleteMany(options);
 
-    // for (const event of events) {
-    //   output.push({
-    //     id: event.id,
-    //     description: event.description,
-    //     dayOfWeek: event.dayOfWeek,
-    //     userId: event.userId as string
-    //   });
-    // }
+    const output: DeleteManyOutput[] = [];
 
-    // return output;
+    for (const event of events) {
+      output.push({
+        id: event.id,
+        description: event.description,
+        dayOfWeek: event.dayOfWeek,
+        userId: event.userId as string
+      });
+    }
+
+    return output;
   }
 }

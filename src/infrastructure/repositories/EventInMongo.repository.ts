800de647f791e82529ff972f601b607d7a920309
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-28 23:44:14 -0300

    feat: add filtering, sort and pagination to EventInMongo repository

--- src/infrastructure/repositories/EventInMongo.repository.ts
+++ src/infrastructure/repositories/EventInMongo.repository.ts
@@ -4,12 +4,17 @@ import Connection from '../database/Connection';
 import EventModel from '../database/event/EventModel';
 
 type QueryObject = {
-  filter?: any;
-  sort?: any;
+  day?: string;
+  description?: string;
+  sort?: string;
   limit?: number;
-  skip?: number;
+  page?: number;
 };
 
+type FilterList = {
+  dayOfWeek?: { $regex: string; $options: 'i' };
+  description?: { $regex: string; $options: 'i' };
+};
 export default class EventInMongoRepository implements IEventRepository {
   constructor(readonly connection: Connection) {}
   async insert(event: Event): Promise<void> {
@@ -19,7 +24,11 @@ export default class EventInMongoRepository implements IEventRepository {
   }
 
   async findAll(options: QueryObject = {}): Promise<Event[]> {
-    const { filter = {}, sort = {}, limit = 10, skip } = options;
+    const { day, description, sort, limit = 7, page = 1 } = options;
+
+    const filter: FilterList = {};
+    if (day) filter.dayOfWeek = { $regex: day as string, $options: 'i' };
+    if (description) filter.description = { $regex: description as string, $options: 'i' };
 
     let query = EventModel.find(filter).sort(sort);
 
@@ -27,9 +36,8 @@ export default class EventInMongoRepository implements IEventRepository {
       query = query.limit(limit);
     }
 
-    if (skip) {
-      query = query.skip(skip);
-    }
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
 
     const docs = await query.exec();
 

797a1a7713ece85854c2244cc79154cd22024a78
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-28 23:46:31 -0300

    feat: add proper parameter and return type to deleteMany method

--- src/core/ports/repositories/Event.repository.ts
+++ src/core/ports/repositories/Event.repository.ts
@@ -4,6 +4,6 @@ export default interface IEventRepository {
   insert(event: Event): Promise<void>;
   findAll(options: object): Promise<Event[]>;
   findById(id: string): Promise<Event | null>;
-  deleteMany(): Promise<void>;
+  deleteMany(options: object): Promise<Event[]>;
   deleteById(id: string): Promise<void>;
 }

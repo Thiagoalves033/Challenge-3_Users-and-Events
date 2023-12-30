2a954aad0af7f8b313caaf471be6f37815995bf1
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-30 19:47:40 -0300

    feat: add public properties to CustomAPIError class

--- src/infrastructure/errors/CustomError.ts
+++ src/infrastructure/errors/CustomError.ts
@@ -1,9 +1,13 @@
 export default class CustomAPIError extends Error {
-  constructor(
-    message: string,
-    private statusCode: number
-  ) {
+  statusCode: number;
+  message: string;
+  type: string;
+
+  constructor(message: string, type: string, statusCode: number) {
     super(message);
+
     this.statusCode = statusCode;
+    this.message = message;
+    this.type = type;
   }
 }

e8bfc53ed608d99864c7697a07fdb97c08d0278f
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-30 19:54:24 -0300

    feat: create Validator middleware

---
+++ src/infrastructure/middlewares/Validator.middleware.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+import { Schema, ValidationResult } from 'joi';
+import ValidationError from '../errors/Validation.error';
+
+export function Validator(schema: Schema) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const result: ValidationResult = schema.validate(req.body, { abortEarly: false });
+
+    if (result.error) {
+      throw new ValidationError(result);
+    }
+
+    next();
+  };
+}

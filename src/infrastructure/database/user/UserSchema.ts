b4cb9547210fc81b36ac9efcf45518c5c78d1fcb
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-30 19:40:25 -0300

    feat: add custom validation messages to user schema

--- src/infrastructure/database/user/UserSchema.ts
+++ src/infrastructure/database/user/UserSchema.ts
@@ -2,13 +2,23 @@ import { Schema } from 'mongoose';
 import IUser from './UserInterface';
 
 const UserSchema = new Schema<IUser>({
-  firstName: { type: String, required: true, minlength: 5 },
-  lastName: { type: String, required: true, minlength: 5 },
-  birthDate: { type: String, required: true },
-  city: { type: String, required: true },
-  country: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  firstName: {
+    type: String,
+    required: [true, 'firstName is required'],
+    minlength: [5, 'Minimum length for firstName is 5']
+  },
+
+  lastName: {
+    type: String,
+    required: [true, 'lastName is required'],
+    minlength: [5, 'Minimum length for lastName is 5']
+  },
+
+  birthDate: { type: String, required: [true, 'birthDate is required'] },
+  city: { type: String, required: [true, 'city is required'] },
+  country: { type: String, required: [true, 'country is required'] },
+  email: { type: String, required: [true, 'email is required'], unique: true },
+  password: { type: String, required: [true, 'password is required'] }
 });
 
 export default UserSchema;

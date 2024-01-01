d1836adaca616ab0c5ce4f395e602f9819602eed
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-31 22:52:07 -0300

    test: create DeleteOneEvent usecase unit test

---
+++ test/unit/event/DeleteOneEvent.use-case.test.ts
@@ -0,0 +1,38 @@
+import DeleteOneEvent from '../../../src/core/usecases/event/DeleteOneEvent.use-case';
+
+const eventRepoMock = {
+  insert: jest.fn(),
+  findAll: jest.fn(),
+  findById: jest.fn(),
+  deleteMany: jest.fn(),
+  deleteById: jest.fn()
+};
+
+const singleEventMock = {
+  id: 'id1',
+  description: 'description',
+  dayOfWeek: 'sunday',
+  userId: 'id'
+};
+
+const deleteOneEvent = new DeleteOneEvent(eventRepoMock);
+
+describe('DeleteOneEvent usecase', () => {
+  it('Should throw an error if no event is found', async () => {
+    eventRepoMock.findById.mockResolvedValueOnce(null);
+
+    const eventFound = async () => {
+      await deleteOneEvent.execute(singleEventMock.id);
+    };
+
+    expect(eventFound).rejects.toThrow('Could not find event');
+  });
+
+  it('Should delete the event if found', async () => {
+    eventRepoMock.findById.mockResolvedValueOnce(singleEventMock);
+
+    await deleteOneEvent.execute(singleEventMock.id);
+
+    expect(eventRepoMock.deleteById).toHaveBeenCalledWith(singleEventMock.id);
+  });
+});

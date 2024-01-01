e23d3120e845e5c8b2d4e9ad851c9e112dbd2f47
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-31 22:49:26 -0300

    test: create UserSignIn usecase unit test

---
+++ test/unit/user/UserSignIn.use-case.test.ts
@@ -0,0 +1,62 @@
+import User from '../../../src/core/entities/User.entity';
+import UserSignIn from '../../../src/core/usecases/user/UserSignIn.use-case';
+
+const userRepoMock = {
+  findByEmail: jest.fn(),
+  findById: jest.fn(),
+  insert: jest.fn()
+};
+
+const encrypterMock = {
+  encrypt: jest.fn(),
+  compare: jest.fn()
+};
+
+const tokenProviderMock = {
+  generate: jest.fn(),
+  verify: jest.fn()
+};
+
+const userMock = {
+  firstName: 'first name',
+  lastName: 'last name',
+  birthDate: 'birthdate',
+  city: 'city',
+  country: 'country',
+  email: 'email',
+  password: 'password'
+};
+
+const userSignIn = new UserSignIn(userRepoMock, encrypterMock, tokenProviderMock);
+
+describe('UserSignIn usecase', () => {
+  it('Should throw an error if user is not registered', async () => {
+    userRepoMock.findByEmail.mockResolvedValueOnce(null);
+
+    const existingUser = async () => {
+      await userSignIn.execute({ email: userMock.email, password: userMock.password });
+    };
+
+    expect(existingUser).rejects.toThrow('Email already in use');
+  });
+
+  it('Should throw an error if password is invalid', async () => {
+    userRepoMock.findByEmail.mockResolvedValueOnce(new User(userMock));
+    encrypterMock.compare.mockResolvedValueOnce(false);
+
+    const invalidPass = async () => {
+      await userSignIn.execute(new User(userMock));
+    };
+
+    expect(invalidPass).rejects.toThrow('Invalid Password');
+  });
+
+  it('Should generate an output with a token', async () => {
+    userRepoMock.findByEmail.mockResolvedValueOnce(new User(userMock));
+    encrypterMock.compare.mockResolvedValueOnce(true);
+
+    const output = await userSignIn.execute(new User(userMock));
+
+    expect(output).toHaveProperty('token');
+  });
+});

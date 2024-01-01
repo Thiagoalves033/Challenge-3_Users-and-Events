e6710a7ef0a60fed975c5dd9c10334a63b4ecaa6
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-31 22:51:17 -0300

    test: create FindAllEvents usecase unit test

---
+++ test/unit/event/FindAllEvents.use-case.test.ts
@@ -0,0 +1,46 @@
+import FindAllEvents from '../../../src/core/usecases/event/FindAllEvents.use-case';
+
+const eventRepoMock = {
+  insert: jest.fn(),
+  findAll: jest.fn(),
+  findById: jest.fn(),
+  deleteMany: jest.fn(),
+  deleteById: jest.fn()
+};
+
+const eventArrayMock = [
+  {
+    id: 'id1',
+    description: 'description',
+    dayOfWeek: 'sunday',
+    userId: 'id'
+  },
+
+  {
+    id: 'id2',
+    description: 'description',
+    dayOfWeek: 'sunday',
+    userId: 'id'
+  }
+];
+
+const findAllEvents = new FindAllEvents(eventRepoMock);
+
+describe('FindAllEvents usecase', () => {
+  it('Should return an array of found events', async () => {
+    eventRepoMock.findAll.mockResolvedValueOnce(eventArrayMock);
+
+    const events = await findAllEvents.execute({});
+
+    expect(events).toEqual(expect.any(Array));
+    expect(events.length).toBe(2);
+    expect(events[0]).toEqual(
+      expect.objectContaining({
+        id: 'id1',
+        description: 'description',
+        dayOfWeek: 'sunday',
+        userId: 'id'
+      })
+    );
+  });
+});

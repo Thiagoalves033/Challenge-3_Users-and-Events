89676c92065639aeb885ec708c629f414f9da8fb
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-31 22:51:44 -0300

    test: create FindOneEvent usecase unit test

---
+++ test/unit/event/FindOneEvent.use-case.test.ts
@@ -0,0 +1,45 @@
+import FindOneEvent from '../../../src/core/usecases/event/FindOneEvent.use-case';
+
+const eventRepoMock = {
+  insert: jest.fn(),
+  findAll: jest.fn(),
+  findById: jest.fn(),
+  deleteMany: jest.fn(),
+  deleteById: jest.fn()
+};
+
+const singleEventMock = {
+  id: 'id1',
+  description: 'description',
+  dayOfWeek: 'sunday',
+  userId: 'id'
+};
+
+const findOneEvent = new FindOneEvent(eventRepoMock);
+
+describe('FindOneEvent usecase', () => {
+  it('Should throw an error if no event is found', async () => {
+    eventRepoMock.findById.mockResolvedValueOnce(null);
+
+    const eventFound = async () => {
+      await findOneEvent.execute(singleEventMock.id);
+    };
+
+    expect(eventFound).rejects.toThrow('Could not find event');
+  });
+
+  it('Should return the found event', async () => {
+    eventRepoMock.findById.mockResolvedValueOnce(singleEventMock);
+
+    const event = await findOneEvent.execute(singleEventMock.id);
+
+    expect(event).toEqual(
+      expect.objectContaining({
+        id: 'id1',
+        description: 'description',
+        dayOfWeek: 'sunday',
+        userId: 'id'
+      })
+    );
+  });
+});

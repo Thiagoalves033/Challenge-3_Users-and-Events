8b8d55c612d88e9a49788c2b1677a390aff9b28e
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-31 22:49:48 -0300

    test: add CreateEvent usecase unit test

---
+++ test/unit/event/CreateEvent.use-case.test.ts
@@ -0,0 +1,53 @@
+import Event from '../../../src/core/entities/Event.entity';
+import CreateEvent from '../../../src/core/usecases/event/CreateEvent.use-case';
+
+const eventRepoMock = {
+  insert: jest.fn(),
+  findAll: jest.fn(),
+  findById: jest.fn(),
+  deleteMany: jest.fn(),
+  deleteById: jest.fn()
+};
+
+const eventMock = {
+  description: 'description',
+  dayOfWeek: 'sunday',
+  userId: 'id'
+};
+
+const createEvent = new CreateEvent(eventRepoMock);
+
+describe('CreateEvent usecase', () => {
+  it('Should insert an event with correct userId', async () => {
+    eventRepoMock.findById.mockResolvedValueOnce(new Event(eventMock));
+
+    await createEvent.execute(eventMock);
+
+    expect(eventRepoMock.insert).toHaveBeenCalledWith(expect.any(Event));
+    expect(eventRepoMock.insert).toHaveBeenCalledWith(expect.objectContaining({ userId: 'id' }));
+  });
+
+  it('Should throw an error if event is not saved correctly', async () => {
+    eventRepoMock.findById.mockResolvedValueOnce(null);
+
+    const savedEvent = async () => {
+      await createEvent.execute(eventMock);
+    };
+
+    expect(savedEvent).rejects.toThrow('Could not save event');
+  });
+
+  it('Should return an object with correct properties', async () => {
+    const mockEventUserId = new Event(eventMock);
+    mockEventUserId.setUserId(eventMock.userId);
+
+    eventRepoMock.findById.mockResolvedValueOnce(mockEventUserId);
+
+    const event = await createEvent.execute(eventMock);
+
+    expect(event).toHaveProperty('id');
+    expect(event).toHaveProperty('description', 'description');
+    expect(event).toHaveProperty('dayOfWeek', 'sunday');
+    expect(event).toHaveProperty('userId', 'id');
+  });
+});

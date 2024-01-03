929c538b3ed904112b0d50624f0f7034d93d54ce
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2024-01-03 16:56:03 -0300

    feat: add return output to UserSignUp usecase

--- src/core/usecases/user/UserSignUp.use-case.ts
+++ src/core/usecases/user/UserSignUp.use-case.ts
@@ -3,14 +3,24 @@ import IUserRepository from '../../ports/repositories/User.repository';
 import IPasswordEncrypter from '../../ports/interfaces/PasswordEncrypter.port';
 import UseCase from '../../ports/interfaces/UseCase';
 import EmailAlreadyInUseError from '../../errors/EmailAlreadyInUse.error';
+import NotSavedError from '../../errors/NotSaved.error';
 
-export default class UserSignUp implements UseCase<User, void> {
+type SignUpOutput = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  birthDate: string;
+  city: string;
+  country: string;
+  email: string;
+};
+export default class UserSignUp implements UseCase<User, SignUpOutput> {
   constructor(
     private UserRepo: IUserRepository,
     private Encrypter: IPasswordEncrypter
   ) {}
 
-  async execute(input: User): Promise<void> {
+  async execute(input: User): Promise<SignUpOutput> {
     const existingUser = await this.UserRepo.findByEmail(input.email);
     if (existingUser) throw new EmailAlreadyInUseError();
 
@@ -18,5 +28,20 @@ export default class UserSignUp implements UseCase<User, void> {
 
     const newUser = new User(input);
     await this.UserRepo.insert(newUser);
+
+    const savedUser = await this.UserRepo.findByEmail(newUser.email);
+    if (!savedUser) throw new NotSavedError();
+
+    const output: SignUpOutput = {
+      id: savedUser.id,
+      firstName: savedUser.firstName,
+      lastName: savedUser.lastName,
+      birthDate: savedUser.birthDate,
+      city: savedUser.city,
+      country: savedUser.country,
+      email: savedUser.email
+    };
+
+    return output;
   }
 }

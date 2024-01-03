760a1a2413a1a1ade3e9c3049ab208b92c54dbef
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2024-01-02 23:21:45 -0300

    fix: improve auth middleware to handle 'Bearer' and invalid tokens

--- src/infrastructure/middlewares/UserAuthMiddleware.ts
+++ src/infrastructure/middlewares/UserAuthMiddleware.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import JWTProvider from '../providers/JwtTokenProvider';
 import IUserRepository from '../../core/ports/repositories/User.repository';
+import UnauthorizedError from '../errors/Unauthorized.error';
 
 type TokenPayload = {
   firstName: string;
@@ -10,23 +11,19 @@ type TokenPayload = {
 
 export default function UserAuthMiddleware(UserRepo: IUserRepository) {
   return async (req: Request, res: Response, next: NextFunction) => {
-    const accessDenied = () => res.status(403);
-
-    const token = req.headers.authorization?.replace('Bearer', '');
-
+    let token = req.headers.authorization;
     if (!token) {
-      accessDenied();
-      return;
+      throw new UnauthorizedError('Unauthorized User');
     }
 
+    if (token.includes('Bearer')) token = token.split(' ')[1];
+
     const JwtProvider = new JWTProvider(process.env.JWT_SECRET!);
+    const userToken = JwtProvider.verify(token as string) as TokenPayload;
 
-    const userToken = JwtProvider.verify(token) as TokenPayload;
     const user = await UserRepo.findByEmail(userToken.email);
-
     if (!user) {
-      accessDenied();
-      return;
+      throw new UnauthorizedError('Unauthorized User');
     }
 
     const loggedUser = {

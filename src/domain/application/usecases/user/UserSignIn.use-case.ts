0a9c82bb3f2c094a8b29257ead135606249aefb8
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2024-01-03 23:07:49 -0300

    fix: update UserSignIn usecase to throw the correct error

--- src/domain/application/usecases/user/UserSignIn.use-case.ts
+++ src/domain/application/usecases/user/UserSignIn.use-case.ts
@@ -2,8 +2,7 @@ import UseCase from '../../../../core/ports/UseCase.port';
 import IUserRepository from '../../repositories/User.repository';
 import IEncrypter from '../../../../core/ports/Encrypter.port';
 import ITokenProvider from '../../../../core/ports/TokenProvider.port';
-import EmailAlreadyInUseError from '../errors/EmailAlreadyInUse.error';
-import InvalidPasswordError from '../errors/InvalidPassword.error';
+import InvalidCredentialsError from '../errors/InvalidCredentials.error';
 
 type SignInInput = {
   email: string;
@@ -26,10 +25,10 @@ export default class UserSignIn implements UseCase<SignInInput, SignInOutput> {
 
   async execute(input: SignInInput): Promise<SignInOutput> {
     const existingUser = await this.UserRepo.findByEmail(input.email);
-    if (!existingUser) throw new EmailAlreadyInUseError();
+    if (!existingUser) throw new InvalidCredentialsError();
 
     const samePassword = await this.Encrypter.compare(input.password, existingUser.password);
-    if (!samePassword) throw new InvalidPasswordError();
+    if (!samePassword) throw new InvalidCredentialsError();
 
     const userToken = this.TokenProvider.generate({
       firstName: existingUser.firstName,

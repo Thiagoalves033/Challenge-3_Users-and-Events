df91ec597b3b32e9099141e68d67cf7183c0dfa2
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2024-01-03 23:08:30 -0300

    refactor: update main imports to reflect recent code restructuring

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
 import 'express-async-errors';
+
 import express, { Express } from 'express';
-import ErrorHandler from './infrastructure/middlewares/ErrorHandler.middleware';
-import userRouter from './infrastructure/routes/user.route';
-import eventRouter from './infrastructure/routes/event.route';
+import ErrorHandler from './infrastructure/express/middlewares/ErrorHandler.middleware';
+import userRouter from './infrastructure/express/routes/user.route';
+import eventRouter from './infrastructure/express/routes/event.route';
+
 import swaggerUi from 'swagger-ui-express';
 import swaggerConfig from './infrastructure/swagger.json';
 
 const app: Express = express();
+
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 app.use('/api/v1/users', userRouter);
